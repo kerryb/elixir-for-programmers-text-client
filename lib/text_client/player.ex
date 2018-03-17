250f42930b3ef7ffcd6c97bad68a9ec642df8573
Kerry Buckley <kerryjbuckley@gmail.com>
2018-03-17 18:51:24 +0000

    Fix game state handling in Player

--- lib/text_client/player.ex
+++ lib/text_client/player.ex
@@ -1,23 +1,23 @@
 defmodule TextClient.Player do
   alias TextClient.{Move, Prompt, State, Summary}
 
-  def play(%State{tally: %{game_state: :good_guess}} = game) do
+  def play(%State{tally: %{state: :good_guess}} = game) do
     continue_with_message(game, "Good guess!")
   end
 
-  def play(%State{tally: %{game_state: :bad_guess}} = game) do
+  def play(%State{tally: %{state: :bad_guess}} = game) do
     continue_with_message(game, "Nope, sorry.")
   end
 
-  def play(%State{tally: %{game_state: :already_guessed}} = game) do
+  def play(%State{tally: %{state: :already_guessed}} = game) do
     continue_with_message(game, "You already tried that letter.")
   end
 
-  def play(%State{tally: %{game_state: :won}}) do
+  def play(%State{tally: %{state: :won}}) do
     exit_with_message("Congratulations, you won!")
   end
 
-  def play(%State{tally: %{game_state: :lost}}) do
+  def play(%State{tally: %{state: :lost}}) do
     exit_with_message("Oh dear, you lost.")
   end
 
@@ -35,7 +35,7 @@ defmodule TextClient.Player do
 
   defp continue_with_message(game, message) do
     IO.puts(message)
-    play(game)
+    continue(game)
   end
 
   defp exit_with_message(message) do
